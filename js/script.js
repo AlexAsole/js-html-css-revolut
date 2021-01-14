b5f134642fb2c281ab66dc62f2f62c29abc7b569
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-01-14 17:18:30 +0100

    js resettato

--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
 $(document).ready(function(){
-//   $('.btn-menu').click(function(){
-//     if($(this).find('.menu').hasClass('active') === false){
-//       $('.btn-menu').find('.menu').removeClass('active');
-//       $(this).find('.menu').addClass('active')
-//     } else {
-//       $(this).find('.menu').removeClass('active')
-//     }
-// })
-// Ho cambiato, passando a next piuttosto che find perché il menù che si apre se ci clicco sopra,
-// essendo parte del <li> si richiude e non so come evitarlo se non così 
-$('.btn-menu').click(function(){
-  if($(this).next().hasClass('active') === false){
-    $('.btn-menu').next().removeClass('active');
-    $(this).next().addClass('active')
-  } else {
-    $(this).next().removeClass('active')
-  }
+  $('.btn-menu').click(function(){
+    if($(this).find('.menu').hasClass('active') === false){
+      $('.btn-menu').find('.menu').removeClass('active');
+      $(this).find('.menu').addClass('active')
+    } else {
+      $(this).find('.menu').removeClass('active')
+    }
 })
+
 })
